8183d729f35326c123d992680fa99b9a8d123887
Sean Fahey <seanfahey10@gmail.com>
2023-08-25 18:00:26 +0000

    running

--- chess7/chess5/settings.py
+++ chess7/chess5/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-h!#_0nk!kpccq_xs9w4rf$4g1s&c&2d=7dm88sa=ek1f768z0f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Default email setup
 DEFAULT_FROM_EMAIL = "manojlovic.mihailo@gmail.com"
