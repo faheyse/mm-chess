5fb9926e544d4dd38ff656359e204d7bacadb1bb
Sean Fahey <seanfahey10@gmail.com>
2023-08-26 14:46:40 +0000

    fix run.sh

--- run.sh
+++ run.sh
@@ -2,14 +2,16 @@
 
 sudo apt -y install python3
 sudo cp /bin/python3 /bin/python
+sudo apt install -y pip
 pip install psycopg2-binary
 sudo apt install -y nginx
 
-git config --global user.email "seanfahey10@gmail.com"
-git config --global user.name "Sean Fahey"
+#git config --global user.email "seanfahey10@gmail.com"
+#git config --global user.name "Sean Fahey"
 
 # Enable and start nginx service
 sudo systemctl enable nginx
+
 sudo systemctl start nginx
 
 # Navigate to your project directory
@@ -21,13 +23,11 @@ cd ~/mm-chess/chess7
 # Install project dependencies
 pip3 install -r requirements.txt
 
-# Run Gunicorn to serve your Django application in detached mode
-#gunicorn chess5.wsgi:application --bind 127.0.0.1:8000 --daemon
-python manage.py runserver 127.0.0.1:8000
-# Place the Nginx configuration file in the sites-available directory
-# Replace 'your_nginx_config' with the actual path to your Nginx config file
-sudo rm /etc/nginx/sites-enabled/mm-chess.conf
-sudo cp ~/mm-chess/mm-chess.conf /etc/nginx/sites-available/mm-chess.conf
+
+sudo rm /etc/nginx/sites-enabled/*
+sudo rm /etc/nginx/sites-available/*
+
+sudo cp ~/mm-chess/mm-chess.conf /etc/nginx/sites-available/
 
 # Create a symbolic link to enable the site
 sudo ln -s /etc/nginx/sites-available/mm-chess.conf /etc/nginx/sites-enabled/
@@ -38,3 +38,7 @@ sudo nginx -t
 # Restart Nginx for changes to take effect
 sudo systemctl restart nginx
 
+python manage.py runserver 127.0.0.1:8000
+
+
+sudo systemctl stop nginx
