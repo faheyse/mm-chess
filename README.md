df11419cae2049c16c888f8fb950b6ab1cce873f
Sean Fahey <seanfahey10@gmail.com>
2023-08-27 19:20:32 +0000

    working server Aug 27

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
+To run the server, you must have python installed:
+sudo apt install -y python3; sudo cp /bin/python3 /bin/python
+
+and nginx:
+sudo apt install nginx
+
+then in the mm-chess/ directory, run:
+./run.sh
+
+the server should now be running
+
+
+to debug on the server this may be helpful:
 sudo tail -f /var/log/nginx/error.log
--- run.sh
+++ run.sh
@@ -24,6 +24,7 @@ sudo rm -f /etc/nginx/sites-available/*
 
 
 # Replace 'PWD' with the current directory in mm-chess.conf
+cd ~/mm-chess
 cp mm-chess.conf.template mm-chess.conf
 current_dir=$(pwd)
 sed -i "s|PWD|$current_dir|g" mm-chess.conf
@@ -31,6 +32,8 @@ sed -i "s|PWD|$current_dir|g" mm-chess.conf
 # Copy Nginx site configuration to available sites
 sudo cp ~/mm-chess/mm-chess.conf /etc/nginx/sites-available/
 rm ~/mm-chess/mm-chess.conf
+
+cd ~/mm-chess/chess7
 
 # Create a symbolic link to enable the site
 sudo ln -s /etc/nginx/sites-available/mm-chess.conf /etc/nginx/sites-enabled/
